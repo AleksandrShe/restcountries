b138b2d892a128e689776e0c37dd1716a4343656
Aleksandr Shepelev <a.shepelev@r77.center-inform.ru>
2021-07-30 17:06:01 +0300

    Initial commit

---
+++ src/main/java/org/example/controller/AppController.java
@@ -0,0 +1,22 @@
+package org.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    // inject via application.properties
+    @Value("${welcome.message}")
+    private String message;
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("message", message);
+
+        return "index";
+    }
+
+}
