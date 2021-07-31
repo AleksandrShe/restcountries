cac7d3494a998067840392913d88823b4c762fb2
Aleksandr Shepelev <a.shepelev@r77.center-inform.ru>
2021-07-31 13:20:51 +0300

    add tests.

--- src/main/java/org/example/controller/AppController.java
+++ src/main/java/org/example/controller/AppController.java
@@ -4,6 +4,7 @@ import org.example.models.Country;
 import org.example.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,7 @@ import java.util.List;
  * @author Aleksandr
  */
 @Controller
+@ComponentScan("org.example.repository")
 public class AppController {
 
     @Autowired
--- src/test/java/org/example/controller/AppControllerTest.java
+++ src/test/java/org/example/controller/AppControllerTest.java
@@ -3,32 +3,109 @@ package org.example.controller;
 
 import org.example.models.*;
 import org.example.repository.CountryRepository;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
 
-
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.matchers.JUnitMatchers.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+/**
+ * @author Aleksandr
+ */
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = AppController.class)
 public class AppControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private CountryRepository countryRepository;
+
+    private static final String EXPECTED_STRING = "Country{name=&#39;Russian Federation&#39;," +
+            " population=&#39;146599183&#39;, flag=&#39;https://restcountries.eu/data/rus.svg&#39;, topLevelDomain=[.ru]," +
+            " latlng=[60.0, 100.0], currencies=[Currency(currenciesId=207, code=RUB, name=Russian ruble, symbol=₽)], " +
+            "languages=[Language(languagesId=269, iso639_1=ru, iso639_2=rus, name=Russian, nativeName=Русский)], " +
+            "translations=[Translation(translationsId=186, de=Russland, es=Rusia, fr=Russie, ja=ロシア連邦, it=Russia, " +
+            "br=Rússia, pt=Rússia, nl=Rusland, hr=Rusija, fa=روسیه)], regionalBlocs=[RegionalBloc(regionalBlocsId=148, " +
+            "acronym=EEU, name=Eurasian Economic Union, otherAcronyms=[EAEU], otherNames=[])]}";
+
     @Test
     public void testByName() throws Exception {
 
+        Country russia = provideCountry();
+        given(countryRepository.findByNameIgnoreCase("Russian Federation")).willReturn(russia);
+
+        mockMvc.perform(post("/country")
+                .param("country", "Russian Federation"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("country"))
+                .andExpect(model().attribute("country", equalTo(russia)))
+                .andExpect(content().string(CoreMatchers.containsString(EXPECTED_STRING)));
+    }
+
+    @Test
+    public void testByDomain() throws Exception {
+
+        Country russia = provideCountry();
+        given(countryRepository.findByTopLevelDomain(".ru")).willReturn(russia);
+
+        mockMvc.perform(post("/domain")
+                .param("domain", ".ru"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("country"))
+                .andExpect(model().attribute("country", equalTo(russia)))
+                .andExpect(content().string(CoreMatchers.containsString(EXPECTED_STRING)));
+    }
+
+    @Test
+    public void testByDomainLike() throws Exception {
+
+        Country russia = provideCountry();
+        List<Country> countries = List.of(russia);
+        given(countryRepository.findByTopLevelDomainLike("%r%")).willReturn(countries);
+
+        mockMvc.perform(post("/domainLike")
+                .param("domain", "r"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("countries"))
+                .andExpect(model().attribute("countries", Matchers.contains(russia)))
+                .andExpect(content().string(CoreMatchers.containsString(EXPECTED_STRING)));
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+
+        mockMvc.perform(get("/delete"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+
+        mockMvc.perform(post("/country")
+                .param("country", "Russian Federation"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("country"))
+                .andExpect(model().attribute("country", equalTo(null)))
+                .andExpect(content().string(CoreMatchers.containsString("Country/domain \"<span>Russian Federation</span>\" not found")));
+
+    }
+
+
+    /**
+     * Provide Counry object.
+     */
+    private Country provideCountry() {
         Country russia = new Country();
         russia.setId(186);
         russia.setName("Russian Federation");
@@ -44,13 +121,7 @@ public class AppControllerTest {
         russia.setRegionalBlocs(List.of(new RegionalBloc(148, "EEU",
                 "Eurasian Economic Union", List.of("EAEU"), List.of())));
 
-
-        ResultActions resultActions = mockMvc.perform(get("/country")
-                .param("country", "Russian Federation"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("country"))
-                .andExpect(model().attribute("country", equalTo(russia)))
-                .andExpect(content().string(containsString("Country{name='Russian Federation', population='146599183', flag='https://restcountries.eu/data/rus.svg', topLevelDomain=[.ru], latlng=[60.0, 100.0], currencies=[Currency(currenciesId=207, code=RUB, name=Russian ruble, symbol=₽)], languages=[Language(languagesId=269, iso639_1=ru, iso639_2=rus, name=Russian, nativeName=Русский)], translations=[Translation(translationsId=186, de=Russland, es=Rusia, fr=Russie, ja=ロシア連邦, it=Russia, br=Rússia, pt=Rússia, nl=Rusland, hr=Rusija, fa=روسیه)], regionalBlocs=[RegionalBloc(regionalBlocsId=148, acronym=EEU, name=Eurasian Economic Union, otherAcronyms=[EAEU], otherNames=[])]}")));
+        return russia;
     }
 
 }
